8d66ccf1cdc1386202a3230317ed3bd426423fb3
Hemant <hemant.janrao@wirecard.com>
2020-07-24 17:31:20 +0200

    Added unit test

--- src/test/java/com/kantarmedia/test/SortingApplicationTests.java
+++ src/test/java/com/kantarmedia/test/SortingApplicationTests.java
@@ -1,13 +1,25 @@
 package com.kantarmedia.test;
 
+import com.kantarmedia.test.base.AbstractTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testng.Assert;
 
 @SpringBootTest
-class SortingApplicationTests {
+class SortingApplicationTests extends AbstractTest {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() throws Exception {
+		String uri = "/sort?values=1,2,3";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
+
+		int status = mvcResult.getResponse().getStatus();
+
+		Assert.assertEquals(200, status);
+	}
 }
